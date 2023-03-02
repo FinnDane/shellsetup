52f572d5d670a5bfba5d70b9cc38f20be62a07e7
Finn Dane <finn@finndane.nl>
2023-03-03 00:54:35 +0100

    more curl option

--- shell-setup.sh
+++ shell-setup.sh
@@ -3,11 +3,11 @@ sudo apt install zsh git
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
-curl "${BASEURL}/.zshrc" -O ~/.zshrc
-curl "${BASEURL}/shell-mommy.sh" -O ~/shell-mommy.sh
+curl -fsSL "${BASEURL}/.zshrc" -O ~/.zshrc
+curl -fsSL "${BASEURL}/shell-mommy.sh" -O ~/shell-mommy.sh
 
 #download custom stuff
-curl "${BASEURL}/robbyrussellASCII.zsh-theme" -O ~/.oh-my-zsh/custom/themes/robbyrussellASCII.zsh-theme
+curl -fsSL "${BASEURL}/robbyrussellASCII.zsh-theme" -O ~/.oh-my-zsh/custom/themes/robbyrussellASCII.zsh-theme
 git clone https://github.com/FinnDane/nobitches.git ~/.oh-my-zsh/custom/plugins/nobitches
 
 chsh -s $(which zsh)
